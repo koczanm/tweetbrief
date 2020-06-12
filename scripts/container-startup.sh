ba5c20bbc2360b933bbd9334bce57a1f4c3511c7
Mateusz Koczan <xmateuszkoczan@gmail.com>
2020-06-12 20:55:15 +0200

    Add Cloudformation template + downgrade Python

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set up variables
 cronDays=${BRIEF_PERIOD:-1}
@@ -10,13 +10,13 @@ chmod +x "${scriptPath}/.env.sh"
 
 # Create crontab file in cron directory
 echo "* * */${cronDays} * * ${scriptPath}/tweetbrief-execution.sh >> /var/log/cron.log 2>&1
-# This extra line made it a valid crone" > /etc/cron.d/bot
+# This extra line made it a valid crone" > /etc/cron.d/tweetbrief
 
 # Give execution rights on cron job
-chmod 0644 /etc/cron.d/bot
+chmod 0644 /etc/cron.d/tweetbrief
 
 # Apply cron job
-crontab /etc/cron.d/bot
+crontab /etc/cron.d/tweetbrief
 
 # Create log file
 touch /var/log/cron.log
