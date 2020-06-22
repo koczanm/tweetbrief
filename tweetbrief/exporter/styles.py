402ad7e6bc17ea86e6307c7b9fa0d15b9846054f
Mateusz Koczan <xmateuszkoczan@gmail.com>
2020-06-22 09:39:40 +0200

    Update local deployment (#5)
    * Improve and optimise local deployment
    
    * Update README
    
    * More descriptive title and output filename
    
    * Display tweet's meta (timestamp, RT/likes, @uid, id)
    
    Co-authored-by: Joanna Rutkowska <joanna@golem.foundation>

--- tweetbrief/exporter/styles.py
+++ tweetbrief/exporter/styles.py
@@ -1,23 +1,45 @@
 html_style = """
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans:ExtraCondensed+ExtraLight" />
     <style>
+        h1 {
+            text-align: center;
+            font-family: "Noto Sans";
+            font-style: normal;
+            font-weight: bold;
+            font-size: 14px;
+            line-height: 1;
+        }
+        h2 {
+            text-align: center;
+            font-family: "Noto Sans";
+            font-style: normal;
+            font-weight: normal;
+            font-size: 8px;
+            color: gray;
+            line-height: 1;
+            margin-bottom: 15px;
+        }
         body {
             font-family: "Noto Sans";
             font-style: normal;
             font-weight: normal;
             font-size: 8px;
-            line-height: 1.25;
+            line-height: 1;
         }
         .container {
             columns: 2;
             column-gap: 20px;
             column-fill: balance;
         }
-        .tweet {
+        .tweet-box {
             display: flex;
-            margin-bottom: 10px;
-            padding-bottom: 9px;
+            flex-direction: column;
+            margin-bottom: 5px;
+            padding-bottom: 1px;
             border-bottom: 1px solid black;
+        }
+        .tweet-box-content {
+            display: flex;
         }
         .tweet-text {
             flex: 1;
@@ -26,6 +48,16 @@ html_style = """
         .tweet-qrcode {
             margin-left: 10px;
         }
+        .tweet-stats {
+            display: flex;
+            justify-content: space-between;
+            font-size: 6px;
+            color: gray;
+            text-align: left;
+            margin-top: 3px;
+            margin-bottom: 0px;
+        }
+
     </style>
 """
 
